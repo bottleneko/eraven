50a0efed3dea8a2f39c1a77657812b3c44f22655
Boris Murashov <bottleneko@gmail.com>
2019-06-29 00:41:36 +0300

    Logger stub

---
+++ src/er_logger_handler.erl
@@ -0,0 +1,23 @@
+-module(er_logger_handler).
+
+% Logger callbacks
+-export([log/2, filter_config/1, changing_config/3, adding_handler/1, removing_handler/1]).
+
+%%%===================================================================
+%%% Logger callbacks
+%%%===================================================================
+
+log(_LogEvent, _Config) ->
+  ok.
+
+filter_config(Config) ->
+  Config.
+
+changing_config(_SetOrUpdate, _OldConfig, NewConfig) ->
+  NewConfig.
+
+adding_handler(Config) ->
+  {ok, Config}.
+
+removing_handler(_Config) ->
+  ok.
