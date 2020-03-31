68d74e2ed108a630e3b8c7cedd3e4681d1e0aed7
Oleg Mareskin <olegmareskin@gmail.com>
2020-03-31 16:05:09 +0400

    Processing reports and minor changes

--- src/er_logger_handler.erl
+++ src/er_logger_handler.erl
@@ -3,7 +3,10 @@
 -define(MICROSECONDS_IN_SECONDS, 1000000).
 
 % Logger callbacks
--export([log/2, filter_config/1, changing_config/3, adding_handler/1, removing_handler/1]).
+-export([log/2, changing_config/3]).
+
+-define(UNDEFINED_FUNCTION_REPLACEMENT, <<"Undefined report function">>).
+-define(WRONG_ARITY_REPLACEMENT, <<"Wrong report function arity">>).
 
 %%%===================================================================
 %%% Logger callbacks
@@ -36,32 +39,54 @@ log(#{msg   := Message,
   Fingerprint = maps:get(FingerprintKey, Meta, [<<"{{ default }}">>]),
 
   Context = er_context:new(EnvironmentContext, RequestContext, Extra, UserContext, Tags, #{}, Fingerprint),
-  Event = build_event(format_message(Message), Level, Meta, Context),
-  er_client:send_event(Event, Dsn, JsonEncodeFunction).
-
-filter_config(Config) ->
-  Config.
+  Event = build_event(format_message(Message, Meta), Level, Meta, Context),
+  er_client:send_event(Event, Dsn, JsonEncodeFunction);
+log(LogEvent, HandlerConfig) ->
+  io:format("Eraven log function clause.~nLogEvent: ~p~nHandlerConfig: ~p~n", [LogEvent, HandlerConfig]).
 
 changing_config(update, OldConfig, NewConfig) ->
   OldParams = maps:get(config, OldConfig, #{}),
   NewParams = maps:get(config, NewConfig, #{}),
   {ok, NewConfig#{config => maps:merge(OldParams, NewParams)}}.
 
-adding_handler(Config) ->
-  {ok, Config}.
-
-removing_handler(_Config) ->
-  ok.
-
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
-format_message({string, Message}) ->
+format_message({string, Message}, _Meta) ->
   Message;
-format_message({Format, Data}) ->
+format_message({report, Report}, Meta) ->
+  format_report(Report, Meta);
+format_message({Format, Data}, _Meta) ->
   Formatted = io_lib:format(Format, Data),
   iolist_to_binary(Formatted).
+
+-spec format_report(Report :: map(), Meta :: map()) -> binary().
+format_report(Report, Meta) ->
+  Fun = fun logger:format_otp_report/1,
+  case maps:get(report_cb, Meta, undefined) of
+    undefined ->
+      ?UNDEFINED_FUNCTION_REPLACEMENT;
+    Fun ->
+      case Report of
+        #{report := _, label := _} ->
+          {Format, Arguments} = Fun(Report),
+          unicode:characters_to_binary(io_lib:format(Format, Arguments));
+        _ ->
+          unicode:characters_to_binary(io_lib:format("~p", [Report]))
+      end;
+    ReportFun when is_function(ReportFun, 1) ->
+      {Format, Arguments} = ReportFun(Report),
+      unicode:characters_to_binary(io_lib:format(Format, Arguments));
+    ReportFun when is_function(ReportFun, 2) ->
+      ReportFun(Report, #{
+        depth => 20,
+        chars_limit => 4096,
+        single_line => false}
+      );
+    _ReportFun ->
+      ?WRONG_ARITY_REPLACEMENT
+  end.
 
 -spec build_event(Message, Level, Metadata, Context) -> er_event:t() when
     Message  :: binary(),
